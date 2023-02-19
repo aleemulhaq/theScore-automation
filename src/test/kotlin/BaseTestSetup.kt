6f37928a3626698b2f8bda14c478b39f442c21c0
AleemH <aleemulhaq@gmail.com>
2023-02-19 16:51:00 -0500

    [Auto-2] Adding base classes
    Adding boiler plate for base classes

---
+++ src/test/kotlin/BaseTestSetup.kt
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class BaseTestSetup {
+    @Test
+    fun sampleJunitTest() {
+        Assertions.assertTrue(true);
+
+    }
+}
