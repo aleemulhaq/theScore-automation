47d491adaa6db1e6aefce3ae1c2b052c707d61ac
AleemH <aleemulhaq@gmail.com>
2023-02-21 11:42:13 -0500

    [Auto-13] Adding dynamic devices
    Now the tests will always run, as long as there is a valid real device or emulator attached to the system

--- src/test/kotlin/BaseTestSetup.kt
+++ src/test/kotlin/BaseTestSetup.kt
@@ -6,23 +6,28 @@ import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.TestInstance
+import java.io.BufferedReader
 import java.io.File
+import java.io.IOException
+import java.io.InputStreamReader
 import java.net.URL
+
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 open class BaseTestSetup : Logging {
     protected var driver: AndroidDriver? = null
     private val service: AppiumDriverLocalService = AppiumDriverLocalService.buildDefaultService()
+    private var firstAndroidAdbDeviceID: String? = null
 
     @BeforeAll
     fun setup() {
-        setUpLogging() // logger setup here as this is entry points of test run
-        appiumServiceStart() // start appium 2.0 service
-
-        logger.trace("Initiating tests")
         try {
+            setUpLogging() // logger setup here as this is entry points of test run
+            appiumServiceStart() // start appium 2.0 service
+            logger.trace("Initiating tests")
             // we can add conditional here for optional iphone testing setup
             driver = appiumAndroidSetup()
+            logger.info("Appium Driver capabilities: {${driver?.capabilities}")
             val popupModals = PopupModals(driver)
             assertTrue(popupModals.waitForSplashScreenEnd())
         } catch (e: Exception) {
@@ -47,18 +52,59 @@ open class BaseTestSetup : Logging {
 
     // android driver setup
     private fun appiumAndroidSetup() : AndroidDriver? {
+        getListOfConnectedAndroidDevices()
 
         val directory = File("");
         val appPath = directory.absolutePath + "/theScore.apk"
         val options = UiAutomator2Options()
             .setPlatformName("Android")
             .setAutomationName("UiAutomator2")
-            .setAvd("Pixel_6_API_33") // we can eventually pass device udid's through command line properties/args
             .setApp(appPath)
             .setAutoGrantPermissions(true)
+
+        if (firstAndroidAdbDeviceID == "" || firstAndroidAdbDeviceID == null) {
+            options.setAvd("Pixel_6_API_33") // default to pixel 6 if adb devices list failed
+        }
+        else if (firstAndroidAdbDeviceID!!.contains("emulator")) {
+            options.setUdid(firstAndroidAdbDeviceID) // if it is some other emulator
+        }
+        else{
+            options.setUdid(firstAndroidAdbDeviceID) // else, its a hardware device
+        }
 
         val serviceUrl = "http://127.0.0.1:4723"
         logger.info("Initializing Android Uiautomator2 driver at: $serviceUrl")
         return AndroidDriver( URL(serviceUrl), options)
+    }
+
+    private fun getListOfConnectedAndroidDevices() {
+        val cmd = "\$ANDROID_HOME/platform-tools/adb devices -l"
+        val processBuilder = ProcessBuilder()
+        processBuilder.command("bash", "-c", cmd)
+        try {
+            val process = processBuilder.start()
+            val output = StringBuilder()
+            val reader = BufferedReader(InputStreamReader(process.inputStream))
+            var line: String = reader.readText()
+            output.append(line)
+            val exitVal = process.waitFor()
+            if (exitVal == 0) {
+                logger.info("Successfully grabbed list of devices")
+                var deviceIds = output.lines()?.mapNotNull {
+                    val matches = """^([a-zA-Z0-9\-]+)(\s+)(device)""".toRegex().find(it)
+                    matches?.groupValues?.get(1)
+                }
+                if (deviceIds != null) {
+                    firstAndroidAdbDeviceID = deviceIds[0]
+                }
+//                exitProcess(0)
+            } else {
+                logger.fatal("Command line process to fetch adb device list, failed to exit 0.")
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+        }
     }
 }
