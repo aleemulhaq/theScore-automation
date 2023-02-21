9cff43a0f82338ddc922b2894ca9ce18f1c8317c
AleemH <aleemulhaq@gmail.com>
2023-02-20 21:45:56 -0500

    [Auto-9] Adding parameterized tests
    Adding junit parameterized setup with api url constants

---
+++ src/main/kotlin/SportsApiConstants.kt
@@ -0,0 +1,5 @@
+
+const val SOCCER_TEAM_API_URL= "https://api.thescore.com/soccer/teams/62/profile"
+const val BASKETBALL_TEAM_API_URL = "https://api.thescore.com/basketball/teams/1/profile"
+const val HOCKEY_TEAM_API_URL = "https://api.thescore.com/hockey/teams/25/profile"
+const val FOOTBALL_TEAM_API_URL = "https://api.thescore.com/football/teams/18/profile"
--- src/test/kotlin/TeamTest.kt
+++ src/test/kotlin/TeamTest.kt
@@ -2,8 +2,11 @@ import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeAll
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
+    @DisplayName("Pass the method parameters provided by the @ValueSource annotation")
 class TeamTest: BaseTestSetup() {
     @BeforeAll
     fun navigateThroughOnboarding(){
@@ -16,14 +19,15 @@ class TeamTest: BaseTestSetup() {
         val popupModals = PopupModals(driver)
         assertTrue(popupModals.dismissPopupModal())
     }
-    
-    @Test
-    fun navigateToTeamStats() {
+
+    @ParameterizedTest
+    @DisplayName("Should pass a non-null message to our test method")
+    @ValueSource(strings = [SOCCER_TEAM_API_URL, BASKETBALL_TEAM_API_URL, HOCKEY_TEAM_API_URL, FOOTBALL_TEAM_API_URL])
+    fun `verify team stats displayed`(apiUrl : String) {
         val topNavBar = TopNavBar(driver)
         assertTrue(topNavBar.tapSearchBar())
-        val teamDataClass = TeamDataClasses()
+        val teamDataClass = TeamDataClasses(apiUrl)
         val teamSearchName = teamDataClass.getApiTeamProfile()
-//        val teamSearchName = "Boston Celtics"
         val searchView = SearchView(driver)
         assertTrue(searchView.sendKeysSearchBar(teamSearchName))
         assertTrue(searchView.tapSearchResult())
@@ -36,11 +40,6 @@ class TeamTest: BaseTestSetup() {
         assertTrue(topNavBar.tapBackButton())
         assertEquals(teamSearchName, searchView.getSearchBarText())
     }
-
-//    @Test
-//    fun `caveat of using var`() {
-//    assertTrue(true)
-//    }
 
     @AfterEach
     fun navigateBackToHomePage() {
